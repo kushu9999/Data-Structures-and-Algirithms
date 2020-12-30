7d96c1832a0363b56dbe563f71609a0d8d942637
Kushal Dulani <38341722+kushu9999@users.noreply.github.com>
2020-12-30 16:15:55 +0530

    Add files via upload

---
+++ Random Questions Solving/single_number_1_#136L.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // method 1 XOR
+    vector<int>v={1,2,1,2,4};
+    
+    int ans=0;
+    for(auto i:v)
+    {
+        ans ^= i;
+    }
+    cout<<ans;
+    
+    
+    return 0;
+}
+/*
+method 2 HashMap
+
+use hashmap & store values
+check if that element already avaialable in hashmap 
+then remove that value else add in it
+
+unordered_set<int> us ;
+auto ptr = v.begin();
+for(auto i:v)
+{
+    if( us.find(i)==us.end() )
+    {
+        us.insert(i);
+    }
+    else
+    {
+        us.erase(i);
+    }  
+}
+for(auto i:us)
+{
+    cout<<i<<" ";
+}
+*/
